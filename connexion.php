f66337425c6f8241a6d8167c5fb767d29fb72f3b
Bouchikhi Taha <bouchikhi.taha@gmail.com>
2018-11-24 22:28:30 +0100

    ajout page connexion

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+//Charger lentete du site
+require_once("header.html");
+//connexion a la BDD pour verifier les id
+require_once("db/Db.php");
+//afficher la page
+require_once("Connexion/View/Connexion.phtml");
+?>
