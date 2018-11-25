c90748449c3bc415c34d37b8ce02cbcc67cd88f3
Bouchikhi Taha <bouchikhi.taha@gmail.com>
2018-11-26 00:14:04 +0100

    ajout page commentaire

---
+++ modifierInfo.php
@@ -0,0 +1,8 @@
+<?php
+//Charger lentete du site
+require_once("header.html");
+//connexion a la BDD pour changer les infos
+require_once("db/Db.php");
+//afficher la page
+require_once("ModificationInformationsUtilisateur/View/Modification.phtml");
+?>
